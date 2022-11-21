86448e7ad518c9c8c0eb8ba9f9ae9f4ec9ec9657
z.adhamov <zakhiriddin.adhamov@botkin.ai>
2022-11-21 16:41:28 +0300

    Try use cloud back

--- mini-chat-frontend/src/utils/socket/socket.ts
+++ mini-chat-frontend/src/utils/socket/socket.ts
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 
-// https://s71hn7.deta.dev/ 
+// const socket = io("https://s71hn7.deta.dev", {
+//   path: "/ws/socket.io"
+// });
+
 
 const socket = io("http://127.0.0.1:8000", {
   path: "/ws/socket.io"
